602680179704949a6e435e8c09ea2da71fa63125
Oliver Ling (the Esteemed) <75524491+Draggie306@users.noreply.github.com>
2025-10-25 14:00:07 +0100

    feat: improve text display in render hierarchy

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 pygame.font.init()
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
+my_font = pygame.font.Font(os.path.join("assets", "fonts", "impact.ttf"), 30)
 
 centre_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 pygame.display.set_caption(title="Medieval Fantasy Tower Defense")
@@ -42,13 +42,6 @@ while running:
     keys = pygame.key.get_pressed()
 
     # -- Player cannot move!
-    if keys[pygame.K_w]:
-        text_surface = my_font.render(
-            "This is a **tower defense**, there is no moving!", 
-            True, 
-            (0, 0, 0),
-            None)
-        screen.blit(text_surface, (((screen.get_width()/2)-200, (screen.get_height()/2)+50)))
     # if keys[pygame.K_s]:
     #     player_pos.y += 300 * dt
     # if keys[pygame.K_a]:
@@ -58,14 +51,26 @@ while running:
 
     # Draw background
     background.draw(screen)
-    gui.draw(screen)
 
     # Draw tower
     tower.draw(screen)
 
+
     # Draw and Update Sprites Array
     # enemy_group.draw()
     enemy_group.update()
+
+    # Text/GUI
+    gui.draw(screen)
+
+    ## VERY IMPORTANT text
+    if keys[pygame.K_w]:
+        text_surface = my_font.render(
+            "This is a **tower defense**, there is no moving!", 
+            True, 
+            (0, 0, 0),
+            None)
+        screen.blit(text_surface, (((screen.get_width()/2)-250, (screen.get_height()/2)-250)))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
